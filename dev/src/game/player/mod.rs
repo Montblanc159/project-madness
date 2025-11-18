06bc0b69b007c9f1a48265952cce1d0344e5b751
GuillaumeZ <guillaume.zaugg@gmail.com>
2025-11-18 11:34:22 +0100

    fix movement jitter

--- dev/src/game/player/mod.rs
+++ dev/src/game/player/mod.rs
@@ -75,32 +75,39 @@ fn player_movement_input(
     keys: Res<ButtonInput<KeyCode>>,
     player_velocity: Single<&mut Velocity, With<Player>>,
     mut walk_cycle_timer: ResMut<WalkCycleTimer>,
-    time: Res<Time>,
 ) {
     let mut velocity = player_velocity.into_inner();
-
-    walk_cycle_timer.timer.tick(time.delta());
 
     velocity.value = GridCoords {
         ..Default::default()
     };
 
+    // walk_cycle_timer.timer.pause();
+
     if keys.pressed(KeyCode::KeyA) {
+        walk_cycle_timer.timer.unpause();
+
         velocity.value = GridCoords {
             x: -1,
             ..Default::default()
         }
     } else if keys.pressed(KeyCode::KeyD) {
+        walk_cycle_timer.timer.unpause();
+
         velocity.value = GridCoords {
             x: 1,
             ..Default::default()
         }
     } else if keys.pressed(KeyCode::KeyW) {
+        walk_cycle_timer.timer.unpause();
+
         velocity.value = GridCoords {
             y: 1,
             ..Default::default()
         }
     } else if keys.pressed(KeyCode::KeyS) {
+        walk_cycle_timer.timer.unpause();
+
         velocity.value = GridCoords {
             y: -1,
             ..Default::default()
@@ -110,13 +117,46 @@ fn player_movement_input(
 
 fn move_player(
     mut query: Query<(&mut GridCoords, &Velocity), With<Player>>,
-    walk_cycle_timer: Res<WalkCycleTimer>,
+    mut walk_cycle_timer: ResMut<WalkCycleTimer>,
+    time: Res<Time>,
 ) {
     for (mut player_grid_coords, velocity) in query.iter_mut() {
-        if walk_cycle_timer.timer.remaining_secs() < JITTER_THRESHOLD {
+        if walk_cycle_timer.timer.remaining_secs() == TICK_DELTA
+            && !walk_cycle_timer.timer.is_paused()
+        {
             let destination = *player_grid_coords + velocity.value;
             *player_grid_coords = destination;
+        } else if walk_cycle_timer.timer.remaining_secs() < JITTER_THRESHOLD {
+            // } else if walk_cycle_timer.timer.just_finished() {
+            walk_cycle_timer.timer.reset();
+            walk_cycle_timer.timer.pause();
         }
+
+        walk_cycle_timer.timer.tick(time.delta());
+    }
+}
+
+fn translate_grid_coords_entities(
+    mut commands: Commands,
+    mut grid_coords_entities: Query<(Entity, &Transform, &GridCoords), Changed<GridCoords>>,
+) {
+    for (entity, transform, grid_coords) in grid_coords_entities.iter_mut() {
+        let destination = bevy_ecs_ldtk::utils::grid_coords_to_translation(
+            *grid_coords,
+            IVec2::splat(GRID_SIZE.into()),
+        )
+        .extend(transform.translation.z);
+
+        let tween = Tween::new(
+            EaseFunction::Linear,
+            Duration::from_secs_f32(TICK_DELTA + JITTER_THRESHOLD),
+            lens::TransformPositionLens {
+                start: transform.translation,
+                end: destination,
+            },
+        );
+
+        commands.entity(entity).insert(TweenAnim::new(tween));
     }
 }
 
@@ -131,34 +171,5 @@ fn translate_transform_to_grid_coords(
             },
             IVec2::splat(GRID_SIZE.into()),
         );
-    }
-}
-
-fn translate_grid_coords_entities(
-    mut commands: Commands,
-    mut grid_coords_entities: Query<(Entity, &Transform, &GridCoords), Changed<GridCoords>>,
-    mut walk_cycle_timer: ResMut<WalkCycleTimer>,
-) {
-    for (entity, transform, grid_coords) in grid_coords_entities.iter_mut() {
-        if walk_cycle_timer.timer.remaining_secs() < JITTER_THRESHOLD {
-            walk_cycle_timer.timer.reset();
-
-            let destination = bevy_ecs_ldtk::utils::grid_coords_to_translation(
-                *grid_coords,
-                IVec2::splat(GRID_SIZE.into()),
-            )
-            .extend(transform.translation.z);
-
-            let tween = Tween::new(
-                EaseFunction::Linear,
-                Duration::from_secs_f32(walk_cycle_timer.timer.remaining_secs()),
-                lens::TransformPositionLens {
-                    start: transform.translation,
-                    end: destination,
-                },
-            );
-
-            commands.entity(entity).insert(TweenAnim::new(tween));
-        }
     }
 }
