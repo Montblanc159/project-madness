23d4bbf1f8a85a8e3a25829713bcdc4d1689abb1
GuillaumeZ <guillaume.zaugg@gmail.com>
2026-01-15 00:17:19 +0100

    refactor/occluder-in-its-own-file

--- dev/src/game/lighting/mod.rs
+++ dev/src/game/lighting/mod.rs
@@ -3,37 +3,28 @@ use bevy_firefly::prelude::*;
 
 use crate::game::{
     camera::{MainCamera, spawn_camera},
-    map::{GRID_SIZE, colliders::Wall},
+    map::GRID_SIZE,
 };
 
 const DEFAULT_INTENSITY: f32 = 1.;
 
+mod occluders;
 pub mod torch;
 
 pub fn plugin(app: &mut App) {
     app.add_systems(Startup, add_config_to_camera.after(spawn_camera));
-    app.add_plugins(torch::plugin);
-    app.add_systems(Update, add_occluder::<Wall>);
+    app.add_plugins((occluders::plugin, torch::plugin));
 }
 
 fn add_config_to_camera(mut commands: Commands, camera: Single<Entity, With<MainCamera>>) {
     let camera = camera.into_inner();
 
     commands.entity(camera).insert(FireflyConfig {
-        // ambient_brightness: 0.2,
+        // ambient_brightness: 0.06,
         softness: None,
         light_bands: Some(16),
         ..default()
     });
-}
-
-fn add_occluder<T: Component>(mut commands: Commands, entities: Query<Entity, Added<T>>) {
-    for entity in entities {
-        commands.entity(entity).insert(Occluder2d::rectangle(
-            GRID_SIZE as f32 + 0.01,
-            GRID_SIZE as f32 + 0.01,
-        ));
-    }
 }
 
 trait LightParameters {
---
+++ dev/src/game/lighting/occluders.rs
@@ -0,0 +1,17 @@
+use bevy::prelude::*;
+use bevy_firefly::prelude::*;
+
+use crate::game::map::{GRID_SIZE, colliders::Wall};
+
+pub fn plugin(app: &mut App) {
+    app.add_systems(Update, add_occluder::<Wall>);
+}
+
+fn add_occluder<T: Component>(mut commands: Commands, entities: Query<Entity, Added<T>>) {
+    for entity in entities {
+        commands.entity(entity).insert(Occluder2d::rectangle(
+            GRID_SIZE as f32 + 0.01,
+            GRID_SIZE as f32 + 0.01,
+        ));
+    }
+}
